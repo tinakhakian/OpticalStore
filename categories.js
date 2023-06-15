0efb854cc01ccaeed44a6b78f91837ef8914cb11
tinakhakian <135725187+tinakhakian@users.noreply.github.com>
2023-06-15 13:25:33 -0400

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,15 @@
+/**
+ * categories.js
+ *
+ * The store's categories are defined in an Array of category Objects. Each
+ * category Object has the following properties:
+ *
+ *  - id: String, a unique category id (e.g., "c1", "c2", "c3")
+ *  - name: String, the human-readable name for the category (e.g., "Perfume")
+ */
+
+window.categories = [
+  { id: "redness", name: "Redness Relief" },
+  { id: "dryness", name: "Dry Eye Relief" },
+  { id: "allergy", name: "Allergy Relief" }
+];
